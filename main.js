9c096cd3e03cde5f8f31c1a540c7d6704b71b2b8
abdallaamrfawzy <abdallamrfe@gmail.com>
2024-07-20 01:45:13 +0300

    update

--- main.js
+++ main.js
@@ -11,3 +11,45 @@ document.addEventListener('click', function(event) {
         dropdown.classList.add('hidden');
     }
 });
+
+
+const ctx = document.getElementById('salesChart').getContext('2d');
+        const salesChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['5k', '10k', '15k', '20k', '25k', '30k', '35k', '40k', '45k', '50k', '55k', '60k'],
+                datasets: [{
+                    label: 'Sales Details',
+                    data: [30, 45, 50, 64, 55, 50, 45, 60, 55, 50, 45, 50],
+                    borderColor: 'rgba(72, 128, 255, 1)',
+                    backgroundColor: 'rgba(72, 128, 255, 0.2)',
+                    borderWidth: 2,
+                    pointRadius: 5,
+                    pointBackgroundColor: 'rgba(72, 128, 255, 1)',
+                    pointBorderColor: '#fff',
+                    pointHoverRadius: 8,
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            callback: function(value) {
+                                return value + '%';
+                            }
+                        }
+                    }
+                },
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                return context.parsed.y + '%';
+                            }
+                        }
+                    }
+                }
+            }
+        });
