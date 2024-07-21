258e4c96b4a534338619beda1f08997e9ebcdcb4
abdallaamrfawzy <abdallamrfe@gmail.com>
2024-07-21 23:35:12 +0300

    update

--- main.js
+++ main.js
@@ -210,17 +210,6 @@ function sidebar() {
     })
 }
 
-
-function toggleLanguageDropdown() {
-    document.getElementById('languageDropdown').classList.toggle('hidden');
-}
-
 function toggleAccountDropdown() {
     document.getElementById('accountDropdown').classList.toggle('hidden');
-}
-
-function changeLanguage(language, flagSrc) {
-    document.getElementById('currentLanguage').textContent = language;
-    document.getElementById('currentFlag').src = flagSrc;
-    toggleLanguageDropdown();
 }
